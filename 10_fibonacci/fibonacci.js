14538166fbae066047d022f6bf709c2dc041a8a6
Will <mail@willkimble.com>
2023-07-31 15:03:54 -0500

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  // fibonacci(4) returns the 4th member of the series: 3  (1, 1, 2, 3)
 
+  if (num <= 0) return 'OOPS';
+  if (num == 0) return 0;
+
+  let previousNum = 1;
+  let secondPreviousNum = 0;
+
+  for (i = 2; i <= num; i++) {
+    let sum = previousNum + secondPreviousNum;
+    secondPreviousNum = previousNum;
+    previousNum = sum;
+  }
+
+  return lastNum;
 };
 
 // Do not edit below this line
