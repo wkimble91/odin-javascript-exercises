487409a52b39473be896e193a17ba50d22b1e85f
Will <mail@willkimble.com>
2023-07-31 14:43:08 -0500

    redid palindroms

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (string) {
-  let newString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
-  return newString.split('').reverse().join('') == newString;
+  const newString = string.toLowerCase().replace(/[^0-9a-z]/g, '');
+  return newString == newString.split('').reverse().join('');
 };
 
 // Do not edit below this line
